8f4f9fcf48fb4ef5495b0abcc1f4c08494a5eec1
sxmsuwadee <60160322@go.buu.ac.th>
2019-02-27 18:17:54 +0700

    work lab05

---
+++ lab05/js/validation.js
@@ -0,0 +1,27 @@
+
+function formValidation () {
+    var uid = document.registform.userid; 
+    var passid = document.registform.passid; 
+    var uname = document.registform.username; 
+    var uadd = document.registform.address; 
+    var ucountry = document. registform.country; 
+    var uzip = document. registform. zip; 
+    var uemail = document. registform.email; 
+    var usex = document. registform.sex;
+    if (validateUserID (uid, 5, 12)) { 
+        if (validatePassword (passid, 7, 12)) { 
+            if (allLetter (uname)) { 
+                if (alphanumeric (uadd)) { 
+                    if (countryselect (ucountry)) { 
+                        if (allnumeric (uzip)) { 
+                            if (validateEmail (uemail)) {
+                                if (validateGender(usex)) {}
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return false;
+    }
